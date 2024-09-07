6565594980e8e8467523a9e15087357d4ac886fa
liujiarong <448736378@qq.com>
2024-09-07 12:00:47 +0800

    🐞 fix(logo): logo更换

--- src/components/FullscreenLoading/index.tsx
+++ src/components/FullscreenLoading/index.tsx
@@ -1,16 +1,21 @@
 import { Icon } from '@lobehub/ui';
-import { LobeChat } from '@lobehub/ui/brand';
+// import { LobeChat } from '@lobehub/ui/brand';
 import { Loader2 } from 'lucide-react';
 import { memo } from 'react';
 import { Center, Flexbox } from 'react-layout-kit';
 import Link from 'next/link';
+import { Image } from 'antd';
 
 
 const FullscreenLoading = memo<{ title?: string }>(({ title }) => {
   return (
     <Flexbox height={'100%'} style={{ userSelect: 'none' }} width={'100%'}>
       <Center flex={1} gap={12} width={'100%'}>
-        <LobeChat size={48} type={'combine'} />
+        {/* <LobeChat size={48} type={'combine'} /> */}
+        <Image
+          src="https://upimage.liujiarong.top/app/thumb.php?img=/i/2023/12/31/12kpwbs.png"
+          width={48}
+        />
         <Center gap={16} horizontal>
           <Icon icon={Loader2} spin />
           {title}
