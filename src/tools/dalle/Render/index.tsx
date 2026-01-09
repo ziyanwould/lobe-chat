350521db1021bbedcf2e14fffe6f058594c2ad79
刘家荣 <godisljr@163.com>
2026-01-09 09:48:58 +0800

    fix docker image

--- src/tools/dalle/Render/index.tsx
+++ src/tools/dalle/Render/index.tsx
@@ -30,18 +30,15 @@ const DallE = memo<BuiltinRenderProps<DallEImageItem[]>>(({ content, messageId }
       {/* 没想好工具条的作用 */}
       {/*<ToolBar content={content} messageId={messageId} />*/}
       <PreviewGroup
-        preview={{
-          // 切换图片时设置
-          onChange: (current: number) => {
-            currentRef.current = current;
-          },
-          // 点击预览显示时设置
-
-          onVisibleChange: (visible: boolean, _prevVisible: boolean, current: number) => {
-            currentRef.current = current;
-          },
-          toolbarAddon: <ActionIcon color={'#fff'} icon={Download} onClick={handleDownload} />,
-        }}
+        preview={
+          {
+            // 点击预览显示时设置当前图片索引
+            onVisibleChange: (visible: boolean, _prevVisible: boolean, current: number) => {
+              currentRef.current = current;
+            },
+            toolbarAddon: <ActionIcon color={'#fff'} icon={Download} onClick={handleDownload} />,
+          } as any
+        }
       >
         <GalleyGrid items={content.map((c) => ({ ...c, messageId }))} renderItem={ImageItem} />
       </PreviewGroup>
